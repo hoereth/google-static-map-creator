f42b92ca593f3895ec47590a23c3a8a31b9b41d6
Michael Hoereth <michael@pentabyte.de>
2018-07-19 08:25:31 +0200

    docs

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Now you are ready to create map urls by calling _de.pentabyte.googlemaps.StaticM
 
 ## Examples
 
-The rest of this document is a list of examples. You will see the source code and the resulting Google Map image.
+The rest of this document is a list of examples. You will see the source code and the resulting Google Map image. Please note: the Google Maps images have been downloaded from Google for documentation purposes only. They are copyrighted by Google and you cannot use them in any other way.
 
 ### Location
 
@@ -39,7 +39,9 @@ StaticMap map = new StaticMap(400, 200, APIKEY);
 map.setLocation("Eiffeltower", 16);
 map.setMaptype(Maptype.hybrid);
 ```
-<img src="https://maps.googleapis.com/maps/api/staticmap?size=400x200&center=Eiffeltower&zoom=16&maptype=hybrid&key=AIzaSyDV7nYgIcNdNaoqzt6pk0yxObvp0bUtH9o"/>
+![Location](src/test/resources/location.png)
+
+Source: `https://maps.googleapis.com/maps/api/staticmap?size=400x200&center=Eiffeltower&zoom=16&maptype=hybrid`
 
 ### Markers
 
@@ -59,7 +61,9 @@ markers.add(notreDame);
 
 map.setMarkers(markers);
 ```
-<img src="https://maps.googleapis.com/maps/api/staticmap?size=400x200&maptype=hybrid&markers=Eiffeltower&markers=color:orange|label:N|48.853%2C2.349983&key=AIzaSyDV7nYgIcNdNaoqzt6pk0yxObvp0bUtH9o"/>
+![Markers](src/test/resources/markers.png)
+
+Source: `https://maps.googleapis.com/maps/api/staticmap?size=400x200&maptype=hybrid&markers=Eiffeltower&markers=color:orange%7Clabel:N%7C48.853%2C2.349983`
 
 ### Custom Markers
 
@@ -75,4 +79,6 @@ StaticMarker m2 = new StaticMarker(50.844782, 6.856730);
 m2.setCustomIconUrl("http://cableparks.info/poi_2.png");
 map.addMarker(m2);
 ```
-<img src="https://maps.googleapis.com/maps/api/staticmap?size=400x200&maptype=hybrid&markers=icon:http:%2F%2Fcableparks.info%2Fpoi.png%7Cscale:2%7C50.844944%2C6.856998&markers=icon:http:%2F%2Fcableparks.info%2Fpoi_2.png%7Cscale:2%7C50.844784%2C6.85673&key=AIzaSyDV7nYgIcNdNaoqzt6pk0yxObvp0bUtH9o"/>
+![Custom Markers](src/test/resources/customMarkers.png)
+
+Source: `https://maps.googleapis.com/maps/api/staticmap?size=400x200&maptype=hybrid&markers=icon:http:%2F%2Fcableparks.info%2Fpoi.png%7Cscale:2%7C50.844944%2C6.856998&markers=icon:http:%2F%2Fcableparks.info%2Fpoi_2.png%7Cscale:2%7C50.844784%2C6.85673`
--- src/test/java/de/pentabyte/googlemaps/ExampleCreator.java
+++ src/test/java/de/pentabyte/googlemaps/ExampleCreator.java
@@ -1,24 +1,37 @@
 package de.pentabyte.googlemaps;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.ClassRule;
 import org.junit.Test;
 
 import de.pentabyte.googlemaps.StaticMap.Maptype;
 
 /**
+ * Will run the tests and download the map images from Google. But only if you
+ * provide the system property {@link #GOOGLEAPI_PROPERTYNAME}.
+ * 
  * @author michael hoereth
- *
  */
 public class ExampleCreator {
-	private static String APIKEY = "AIzaSyDV7nYgIcNdNaoqzt6pk0yxObvp0bUtH9o";
+	private static String GOOGLEAPI_PROPERTYNAME = "GOOGLEAPI";
+	private String googleApiKey;
+
+	@ClassRule
+	public static SystemPropertyPreCondition check = new SystemPropertyPreCondition(GOOGLEAPI_PROPERTYNAME);
 
 	@Test
 	public void createLocation() throws ClientProtocolException, IOException {
-		StaticMap map = new StaticMap(400, 200, APIKEY);
+		StaticMap map = new StaticMap(400, 200, googleApiKey);
 		map.setLocation(new StaticLocation("Eiffeltower"), 16);
 		map.setMaptype(Maptype.hybrid);
 
@@ -27,7 +40,7 @@ public class ExampleCreator {
 
 	@Test
 	public void createMarkers() throws ClientProtocolException, IOException {
-		StaticMap map = new StaticMap(400, 200, APIKEY);
+		StaticMap map = new StaticMap(400, 200, googleApiKey);
 		map.setMaptype(Maptype.hybrid);
 
 		map.addMarker(new StaticMarker("Eiffeltower"));
@@ -42,7 +55,7 @@ public class ExampleCreator {
 
 	@Test
 	public void createCustomMarker() throws ClientProtocolException, IOException {
-		StaticMap map = new StaticMap(400, 200, APIKEY);
+		StaticMap map = new StaticMap(400, 200, googleApiKey);
 		map.setMaptype(Maptype.hybrid);
 
 		StaticMarker m1 = new StaticMarker(50.844943, 6.856998);
@@ -64,18 +77,17 @@ public class ExampleCreator {
 
 		File myFile = new File("src/test/resources/" + filename);
 
-		// CloseableHttpClient client = HttpClients.createDefault();
-		// try (CloseableHttpResponse response = client.execute(new
-		// HttpGet(map.toString()))) {
-		// HttpEntity entity = response.getEntity();
-		// if (response.getStatusLine().getStatusCode() != 200)
-		// throw new RuntimeException(response.getStatusLine().toString());
-		// if (entity != null) {
-		// try (FileOutputStream outstream = new FileOutputStream(myFile)) {
-		// entity.writeTo(outstream);
-		// }
-		// }
-		// }
+		CloseableHttpClient client = HttpClients.createDefault();
+		try (CloseableHttpResponse response = client.execute(new HttpGet(map.toString()))) {
+			HttpEntity entity = response.getEntity();
+			if (response.getStatusLine().getStatusCode() != 200)
+				throw new RuntimeException(response.getStatusLine().toString());
+			if (entity != null) {
+				try (FileOutputStream outstream = new FileOutputStream(myFile)) {
+					entity.writeTo(outstream);
+				}
+			}
+		}
 	}
 
 }
